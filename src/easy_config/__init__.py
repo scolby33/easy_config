31d199f4fe840c39ce9104609a9f8e41e8eea7e0
Scott Colby <scolby33@gmail.com>
2018-09-20 02:25:06 -0700

    Add first loading functionality.
    Default files are unimplemented currently.

--- src/easy_config/__init__.py
+++ src/easy_config/__init__.py
@@ -1,15 +1,24 @@
+import configparser
 import dataclasses
+import os
+from pathlib import Path
+from typing import Any, Dict, Iterable, Optional, TextIO, Type, TypeVar, Union
+
+T = TypeVar('T', bound='EasyConfig')
 
 class _InheritDataclassForConfig(type):
     REQUIRED_CLASS_VARIABLES = ['PRIORITY']
 
     def __new__(meta, name, bases, attrs):
-        if not bases:
-            return super().__new__(meta, name, bases, attrs)
         for varname in meta.REQUIRED_CLASS_VARIABLES:
             if not varname in attrs:
-                raise RuntimeError(f'{varname} must be a defined class variable')
-        return dataclasses.dataclass(super().__new__(meta, name, bases, attrs))
+                # log missing varname
+                break
+        else:  # nobreak--nothing was missing
+            return dataclasses.dataclass(super().__new__(meta, name, bases, attrs))
+
+        # did break--something was missing
+        return super().__new__(meta, name, bases, attrs)
 
 
 class EasyConfig(metaclass=_InheritDataclassForConfig):
@@ -17,9 +26,69 @@ class EasyConfig(metaclass=_InheritDataclassForConfig):
         raise NotImplementedError(f'{self.__class__.__qualname__} must be subclassed')
 
     @classmethod
-    def load(cls):
-        # Load config based on PRIORITY
-        pass
+    def _load_file(cls: Type[T], config_file: Union[Path, TextIO]) -> Dict[str, Any]:
+        # Load a file
+        # Default using configparser, but override this to add other file types
+        config = configparser.ConfigParser()
+        if isinstance(config_file, Path) or isinstance(config_file, str):
+            config.read(config_file)
+        else:
+            config.read_file(config_file)
+
+        values = {}
+        for field in dataclasses.fields(cls):
+            if field.type is int:
+                value = config.getint('default', field.name)
+            elif field.type is float:
+                value = config.getfloat('default', field.name)
+            elif field.type is bool:
+                value = config.getboolean('default', field.name)
+            else:
+                value = config.get('default', field.name)
+
+            values[field.name] = value
+
+        return values
+
+    @classmethod
+    def _load_environment(cls: Type[T]) -> Dict[str, Any]:
+        # load from the environment
+        values = {}
+        for field in dataclasses.fields(cls):
+            if field.name in os.environ:
+                values[field.name] = field.type(os.environ[field.name])
+
+        return values
+
+    @classmethod
+    def _load_dict(cls: Type[T], d: Dict[str, Any]) -> Dict[str, Any]:
+        # load from a dictionary
+        values = {}
+        for field in dataclasses.fields(cls):
+            if field.name in d:
+                values[field.name] = field.type(d[field.name])
+        return values
+
+    @classmethod
+    def load(cls: Type[T], additional_files: Optional[Iterable[Union[Path, TextIO]]]=None, *, parse_files: bool=True, parse_environment: bool=True, **kwargs) -> T:
+        values = {}
+        if parse_files:
+            for f in cls.files:
+                values.update(cls._load_file(f))
+        if additional_files:
+            for f in additional_files:
+                values.update(cls._load_file(f))
+        if parse_environment:
+            values.update(cls._load_environment())
+        values.update(cls._load_dict(kwargs))
+
+        try:
+            return cls(**values)
+        except TypeError as e:
+            if e.args[0].startswith('__init__() missing'):
+                raise TypeError('missing some configuration values') from e
+            else:
+                raise e
 
     def dump(self):
         # Write current config to a file or something
