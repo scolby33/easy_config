cf18ba3c06ed0e60da64cca75672f6b9d47b09ae
Scott Colby <scolby33@gmail.com>
2018-09-22 01:33:20 -0700

    Add more values to example ini.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,12 @@
 from pathlib import Path
+import os
 
 import pytest
 
 
 @pytest.fixture(scope='session')
 def example_ini(tmpdir_factory) -> Path:
-    example_ini_contents = """[MyProgram]\n# comment\nnumber = 3"""
+    example_ini_contents = """[MyProgram]\n# comment\nnumber = 3\nfloaty_number = 5\nflag = False\nword = hello"""
     example_ini_path = tmpdir_factory.mktemp('config').join('example.ini')
     with open(example_ini_path, 'w') as f:
         f.write(example_ini_contents)
