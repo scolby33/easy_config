b364b59f27d61228199dcc8c6491bb97942312c8
Scott Colby <scolby33@gmail.com>
2018-09-22 01:12:28 -0700

    Extract the test config class to avoid repetition.

--- tests/test_easy_config.py
+++ tests/test_easy_config.py
@@ -2,23 +2,24 @@
 
 from easy_config import EasyConfig
 
-class A(EasyConfig):
-    FILES = ['myprogram.ini']
+
+class ExampleConfig(EasyConfig):
+    FILES = None
     NAME = 'MyProgram'
 
     number: int
-    name: str
-    check_bounds: bool = True
 
 
 def test_load_file(example_ini):
-    class TestConfig(EasyConfig):
-        FILES = None
-        NAME = 'MyProgram'
-
-        number: int
-
-    assert TestConfig._load_file(example_ini) == {'number': 3}
-    assert TestConfig._load_file(str(example_ini)) == {'number': 3}
+    assert ExampleConfig._load_file(example_ini) == {'number': 3}
+    assert ExampleConfig._load_file(str(example_ini)) == {'number': 3}
     with open(example_ini, 'r') as f:
-        assert TestConfig._load_file(f) == {'number': 3}
+        assert ExampleConfig._load_file(f) == {'number': 3}
+
+
+def test_load_environment(example_env):
+    assert ExampleConfig._load_environment() == {'number': 3}
+
+
+def test_load_dict():
+    assert ExampleConfig._load_dict({'number': '3', 'unused': 'foo'}) == {'number': 3}
