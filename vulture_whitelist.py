c824138e5fb790768bdc93e2a25d651a2ba0bada
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-09-24 20:52:24 +0200

    Fix vulture

--- vulture_whitelist.py
+++ vulture_whitelist.py
@@ -1,3 +1,5 @@
 example_env  # unused function (tests/conftest.py:22)
 example_env  # unused variable (tests/test_easy_config.py:47)
 example_env  # unused variable (tests/test_easy_config.py:57)
+example_config_env  # unused function (tests/conftest.py:35)
+example_config_env  # unused variable (tests/test_easy_config.py:51)
