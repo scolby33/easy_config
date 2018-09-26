6131d46f429060d9ad0599ad2c35ec8415f4e827
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-09-26 10:45:37 +0200

    Update type annotations

--- src/easy_config/contrib/click.py
+++ src/easy_config/contrib/click.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-"""Test the click wrapper."""
+"""A wrapper for generating options for a :mod:`click` command from an :class:`easy_config.EasyConfig`."""
 
 import dataclasses
 from typing import Callable, Type
@@ -9,11 +9,18 @@ import click
 
 from easy_config import EasyConfig
 
+__all__ = [
+    'args_from_config',
+]
 
-def args_from_config(cls: Type[EasyConfig]) -> Callable[[Callable], Callable]:  # noqa: D202
+# A callable that can take in anything, but gives back nothing
+GenericCallable = Callable[..., None]
+
+
+def args_from_config(cls: Type[EasyConfig]) -> Callable[[GenericCallable], GenericCallable]:  # noqa: D202
     """Build a decorator based on the given easy config class."""
 
-    def decorate(command: Callable) -> Callable:
+    def decorate(command: GenericCallable) -> GenericCallable:
         """Decorate the :mod:`click` command."""
         for field in dataclasses.fields(cls):
             if field.default is dataclasses.MISSING:
