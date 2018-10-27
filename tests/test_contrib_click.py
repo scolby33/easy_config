90fdce892d16408351907b788d760fe57896b109
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-10-28 00:20:55 +0200

    Add tests for config_command

--- tests/test_contrib_click.py
+++ tests/test_contrib_click.py
@@ -6,7 +6,7 @@ import click
 from click.testing import CliRunner
 
 from easy_config import EasyConfig
-from easy_config.contrib.click import args_from_config
+from easy_config.contrib.click import args_from_config, config_command
 
 
 def test_option():  # noqa: D202
@@ -23,6 +23,7 @@ def test_option():  # noqa: D202
     @click.command()
     @args_from_config(ExampleConfig)
     def main(number):
+        """Print the example configuration."""
         click.echo(f'number: {number}')
 
     runner = CliRunner()
@@ -47,6 +48,7 @@ def test_argument():  # noqa: D202
     @click.command()
     @args_from_config(ExampleConfig)
     def main(number):
+        """Print the example configuration."""
         click.echo(f'number: {number}')
 
     runner = CliRunner()
@@ -69,8 +71,33 @@ def test_mixed():  # noqa: D202
     @click.command()
     @args_from_config(ExampleConfig)
     def main(number, floaty_number):
+        """Print the example configuration."""
         click.echo(f'number: {number}')
         click.echo(f'floaty_number: {floaty_number}')
+
+    runner = CliRunner()
+    result = runner.invoke(main, ['4'])
+    assert result.output == 'number: 4\nfloaty_number: 5.0\n'
+
+
+def test_pos_arg():  # noqa: D202
+    """Test building a config object for function."""
+
+    class ExampleConfig(EasyConfig):
+        """Example EasyConfig subclass to test with."""
+
+        FILES = None
+        NAME = 'MyProgram'
+
+        number: int
+        floaty_number: float = 5.0
+
+    @click.command()
+    @config_command(ExampleConfig)
+    def main(example_config: ExampleConfig):
+        """Print the example configuration."""
+        click.echo(f'number: {example_config.number}')
+        click.echo(f'floaty_number: {example_config.floaty_number}')
 
     runner = CliRunner()
     result = runner.invoke(main, ['4'])
