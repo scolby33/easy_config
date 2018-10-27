5bd3e2daf939e4fef2afb0abaf23c50986f4663a
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-10-28 00:19:47 +0200

    Add config_command

--- src/easy_config/contrib/click.py
+++ src/easy_config/contrib/click.py
@@ -11,12 +11,16 @@ from easy_config import EasyConfig
 
 __all__ = [
     'args_from_config',
+    'config_command',
 ]
 
 # declaring types for the decorator
 # https://mypy.readthedocs.io/en/stable/generics.html#declaring-decorators
 FuncType = Callable[..., Any]
 F = TypeVar('F', bound=FuncType)
+
+Func2Type = Callable[[Type[EasyConfig]], Any]
+G = TypeVar('G', bound=Func2Type)
 
 
 def args_from_config(cls: Type[EasyConfig]) -> Callable[[F], F]:  # noqa: D202
@@ -33,5 +37,21 @@ def args_from_config(cls: Type[EasyConfig]) -> Callable[[F], F]:  # noqa: D202
             command = wrapper(command)
 
         return command
+
+    return decorate
+
+
+def config_command(cls: Type[EasyConfig]) -> Callable[[G], G]:  # noqa: D202
+    """Build a decorator based on the given easy config class."""
+
+    def decorate(command: G) -> G:  # noqa: D202
+        """Decorate the :mod:`click` command."""
+
+        @args_from_config(cls)
+        def inner_decorate(**kwargs: Any) -> Any:
+            """Apply the keyword arguments to the EasyConfig class."""
+            return command(cls.load(**kwargs))
+
+        return inner_decorate
 
     return decorate
