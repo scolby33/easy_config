cbb848e983979fe5f95c011d74ecc1a9a2319ebc
Scott Colby <scolby33@gmail.com>
2019-02-02 00:08:34 -0500

    Add lost `from e` back.

--- src/easy_config/__init__.py
+++ src/easy_config/__init__.py
@@ -129,7 +129,7 @@ class EasyConfig(metaclass=_InheritDataclassForConfig):
             except (configparser.NoSectionError, configparser.NoOptionError):
                 pass
             except (TypeError, ValueError) as e:
-                raise ConfigValueCoercionError(f'While reading the configuration file `{config_file if given_path else "UNKNWON"}`, could not coerce value for field `{field.name}` to type `{field.type}`')
+                raise ConfigValueCoercionError(f'While reading the configuration file `{config_file if given_path else "UNKNWON"}`, could not coerce value for field `{field.name}` to type `{field.type}`') from e
 
         return values
 
